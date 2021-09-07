309d2ef69c27f00086727f52998dc1352e5126a8
sarvani-math <84523854+sarvani-math@users.noreply.github.com>
2021-09-07 10:26:03 +0530

    Add files via upload

---
+++ codes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+
+
+# In[3]:
+
+
+a = np.array([11,16]) 
+b = np.array([23,21])
+distance = np.linalg.norm(a-b)
+
+
+# In[4]:
+
+
+distance
+
+
+# In[ ]:
+
+
+
+
